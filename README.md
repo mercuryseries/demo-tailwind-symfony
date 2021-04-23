c6e278e555fa7fa35c9caa1e894ddb09e5d12049
Honore Hounwanou <mercuryseries@gmail.com>
2021-04-23 03:01:40 -0400

    wip

---
+++ README.md
@@ -0,0 +1,3 @@
+# Symfony + Tailwind CSS
+
+Source code of [my YouTube video](https://youtube.com/teachersdunet) on Tailwind CSS Integration with Symfony.
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    './templates/**/*.html.twig',
+    './assets/**/*.css',
+    './assets/**/*.js',
+    './assets/**/*.vue',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
